83329dc4c378ef79c2006ee6be2fc7207d7c6ff6
JeanMarc <37602279+Marc-9@users.noreply.github.com>
2020-04-08 13:45:13 -0700

    Create Hierarchy

---
+++ model/fuelformexample.php
@@ -0,0 +1,21 @@
+<?php
+namespace Model;
+use \File;
+use \Debug;
+
+class FuelFormExample extends \Model {
+
+    public static function log_message($name, $password, $msg) {
+        self::_file_exists_or_create();
+        File::append(DOCROOT, 'fuelformfile.csv', implode(", ", array($name, $password, $msg)) . "\n");
+        return true;
+    }
+
+    private static function _file_exists_or_create() {
+        if (!File::exists(DOCROOT . '/fuelformfile.csv')) {
+            File::create(DOCROOT, 'fuelformfile.csv');
+        }
+    }
+
+}
+?>
