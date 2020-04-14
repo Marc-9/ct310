6ef232291262cdb4f0509f8cc4c5f36e76157599
JeanMarc <37602279+Marc-9@users.noreply.github.com>
2020-04-14 13:00:35 -0700

    Add files via upload

---
+++ views/welcome/aboutus.php
@@ -0,0 +1,26 @@
+
+<link rel="stylesheet" href="../../assets/css/aboutus.css" type="text/css">
+<br>
+
+				<div class="chase">
+					<a href=""><img src="../../assets/img/chase.jpg" alt="Chase"></a>
+					<h3>Chase Howard</h3>
+					<p>A sophomore majoring in Computer Science</p>
+				</div>
+				<br>
+				
+				<div class="frenchy">
+					<a href=""><img src="" alt="Frenchy"></a>
+					<h3>Name</h3>
+					<p>Bio</p>
+				</div>
+				<br>
+				
+				<div class="michael">
+					<a href=""><img src="" alt="Michael"></a>
+					<h3>Name</h3>
+					<p>Bio</p>
+				</div>
+
+			
+
