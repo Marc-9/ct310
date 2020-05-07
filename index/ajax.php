83be8ec86e59a8c75b26a4cc4f838e2a7e1160e7
JeanMarc <37602279+Marc-9@users.noreply.github.com>
2020-05-07 14:13:45 -0600

    Add files via upload

--- index/ajax.php
+++ index/ajax.php
@@ -1,28 +1,54 @@
-<?php 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    header('Method Not Allowed', true, 405);
-    echo "GET method requests are not accepted for this resource";
-    exit;
-}
+<?php
 session_start();
 require('config.php');
-$action = $_POST['action'];
-if($action == "upvote"){
+$db= mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
+
+$arr = $_GET['id'];
+//$arr = $_POST['data'];
+$arr2 = explode("_",$arr);
+$action = $arr2[0];
+$check_num = $db->query("SELECT * FROM comment_actions WHERE userid = ".$_SESSION['id']." AND commentid =$arr2[1]");
+
+if($action == 1){
+	if(mysqli_num_rows($check_num) == 0){
+		$db->query("INSERT INTO `comment_actions` (`userid`, `commentid`, `val`) VALUES ( ".$_SESSION['id'].", $arr2[1], 1)");
+		return True;
+	}
+	else{
+		$check_val = $check_num->fetch_assoc();
+		if($check_val['val'] == 1){
+			echo $check_val['val'];
+			$db->query("UPDATE comment_actions SET val = 0 WHERE commentid = $arr2[1] AND userid = ".$_SESSION['id']." ");
+			return True;
+		}
+		else{
+			$db->query("UPDATE comment_actions SET val = 1 WHERE commentid = $arr2[1] AND userid = ".$_SESSION['id']." ");
+			return True;
+		}
+		
+	}
+}
+
+else if($action == 2){
+	if(mysqli_num_rows($check_num) == 0){
+		$db->query("INSERT INTO `comment_actions` (`userid`, `commentid`, `val`) VALUES ( ".$_SESSION['id'].", $arr2[1], 2)");
+		return True;
+	}
+	else{
+		$check_val = $check_num->fetch_assoc();
+		if($check_val['val'] == 2){
+			$db->query("UPDATE comment_actions SET val = 0 WHERE commentid = $arr2[1] AND userid= ".$_SESSION['id']." ");
+			return True;
+		}
+		else{
+			$db->query("UPDATE comment_actions SET val = 2 WHERE commentid = $arr2[1] AND userid= ".$_SESSION['id']." ");
+			return True;
+		}
+		
+	}
 
 }
-else if($action == "downvote"){
 
-}
-else if($action == "edit"){
 
-}
-
-else if($action == "delete"){
-
-}
-else{
-	return False;
-}
-return True
 
 ?>
