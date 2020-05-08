d6d23a53443ce6ebf7e8ee078ba993fe5d9e857e
JeanMarc <37602279+Marc-9@users.noreply.github.com>
2020-05-08 01:37:32 -0600

    Add files via upload

--- index/ajax.php
+++ index/ajax.php
@@ -2,53 +2,53 @@
 session_start();
 require('config.php');
 $db= mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
-
-$arr = $_GET['id'];
-//$arr = $_POST['data'];
-$arr2 = explode("_",$arr);
-$action = $arr2[0];
-$check_num = $db->query("SELECT * FROM comment_actions WHERE userid = ".$_SESSION['id']." AND commentid =$arr2[1]");
-
-if($action == 1){
-	if(mysqli_num_rows($check_num) == 0){
-		$db->query("INSERT INTO `comment_actions` (`userid`, `commentid`, `val`) VALUES ( ".$_SESSION['id'].", $arr2[1], 1)");
-		return True;
+//$arr = $_GET['id'];
+$action = $_POST['action'];
+$commentid = $_POST['commentid'];
+$userid = $_SESSION['id'];
+$result = $db->query("SELECT * FROM comment_actions WHERE userid = $userid AND commentid = $commentid");
+if(mysqli_num_rows($result) > 0){
+	$check = $result->fetch_assoc();
+	if($action == 1 && $check['val'] == 1){
+		$db->query("UPDATE comment_actions SET val = 0 WHERE userid = $userid AND commentid = $commentid");
+		$db->query("UPDATE user_comment SET score = score-1 WHERE commentid = $commentid");
+		header("Status: 404 Not Found");
 	}
-	else{
-		$check_val = $check_num->fetch_assoc();
-		if($check_val['val'] == 1){
-			echo $check_val['val'];
-			$db->query("UPDATE comment_actions SET val = 0 WHERE commentid = $arr2[1] AND userid = ".$_SESSION['id']." ");
-			return True;
-		}
-		else{
-			$db->query("UPDATE comment_actions SET val = 1 WHERE commentid = $arr2[1] AND userid = ".$_SESSION['id']." ");
-			return True;
-		}
-		
+	else if($action == 1 && $check['val'] == 2){
+		$db->query("UPDATE comment_actions SET val = 1 WHERE userid = $userid AND commentid = $commentid");
+		$db->query("UPDATE user_comment SET score = score+2 WHERE commentid = $commentid");
+	 	header("Status: 401 Not Found");
+	 }
+	else if($action == 2 && $check['val'] == 1){
+		$db->query("UPDATE comment_actions SET val = 2 WHERE userid = $userid AND commentid = $commentid");
+		$db->query("UPDATE user_comment SET score = score-2 WHERE commentid = $commentid");
+	 	header("Status: 400 Not Found");
 	}
+	else if($action == 2 && $check['val'] == 2){
+		$db->query("UPDATE comment_actions SET val = 0 WHERE userid = $userid AND commentid = $commentid");
+		$db->query("UPDATE user_comment SET score = score+1 WHERE commentid = $commentid");
+	 	header("Status: 402 Not Found");
+	 }
+	 else if($action == 1 && $check['val'] == 0){
+		$db->query("UPDATE comment_actions SET val = 1 WHERE userid = $userid AND commentid = $commentid");
+		$db->query("UPDATE user_comment SET score = score+1 WHERE commentid = $commentid");
+	 	header("Status: 403 Not Found");
+	 }
+	 else if($action == 2 && $check['val'] == 0){
+		$db->query("UPDATE comment_actions SET val = 2 WHERE userid = $userid AND commentid = $commentid");
+		$db->query("UPDATE user_comment SET score = score-1 WHERE commentid = $commentid");
+	 	header("Status: 405 Not Found");
+	 }
+
 }
-
+else if($action == 1){
+	$db->query("INSERT INTO `comment_actions` (`userid`, `commentid`, `val`) VALUES ( $userid, $commentid, 1)");
+	$db->query("UPDATE user_comment SET score = score + 1 WHERE commentid = $commentid");
+}
 else if($action == 2){
-	if(mysqli_num_rows($check_num) == 0){
-		$db->query("INSERT INTO `comment_actions` (`userid`, `commentid`, `val`) VALUES ( ".$_SESSION['id'].", $arr2[1], 2)");
-		return True;
-	}
-	else{
-		$check_val = $check_num->fetch_assoc();
-		if($check_val['val'] == 2){
-			$db->query("UPDATE comment_actions SET val = 0 WHERE commentid = $arr2[1] AND userid= ".$_SESSION['id']." ");
-			return True;
-		}
-		else{
-			$db->query("UPDATE comment_actions SET val = 2 WHERE commentid = $arr2[1] AND userid= ".$_SESSION['id']." ");
-			return True;
-		}
-		
-	}
-
+	$db->query("INSERT INTO `comment_actions` (`userid`, `commentid`, `val`) VALUES ( $userid, $commentid, 2)");
+	$db->query("UPDATE user_comment SET score = score - 1 WHERE commentid = $commentid");
 }
-
 
 
 ?>
