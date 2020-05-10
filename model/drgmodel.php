30d06e943b6844adadc0dd473e77b69efecca1c5
chasehoward85 <chasehoward85@gmail.com>
2020-05-09 21:59:07 -0600

    Update drgmodel.php

--- model/drgmodel.php
+++ model/drgmodel.php
@@ -6,7 +6,7 @@ class DRGModel extends \Model {
 		return DB::query('SELECT DISTINCT id, drg_definition FROM drg ORDER BY id', DB::SELECT)->execute()->as_array();
     }
     public static function get_hospitals($drg) {
-		return DB::query("SELECT DISTINCT drg_id, drg.drg_definition, average_covered_charges, average_total_payments, average_medicare_payments, hospital.name, hospital.state, hospital.id FROM financials INNER JOIN hospital ON financials.hospital_id = hospital.id INNER JOIN drg ON financials.drg_id = drg.id WHERE drg_Number=$drg", DB::SELECT)->execute()->as_array();
+		return DB::query("SELECT DISTINCT drg_id, drg.drg_definition, average_covered_charges, average_total_payments, average_medicare_payments, hospital.name, hospital.state, hospital.id FROM financials INNER JOIN hospital ON financials.hospital_id = hospital.id INNER JOIN drg ON financials.drg_id = drg.id WHERE drg_id=$drg", DB::SELECT)->execute()->as_array();
 	}
 	
 }
