4488e14634c75bf3f1b5869eed9db6d9a6a7a3f0
michaelbauers <62127209+michaelbauers@users.noreply.github.com>
2020-05-10 22:30:11 -0600

    Update hospitalmodel.php

--- model/hospitalmodel.php
+++ model/hospitalmodel.php
@@ -6,7 +6,7 @@ class HospitalModel extends \Model {
 		return DB::query('SELECT DISTINCT `provider_id`,`provider_name`,`provider_state` From hospital_data ORDER BY `provider_id`', DB::SELECT)->execute()->as_array();
     }
      public static function get_hospitals($id) {
-		return DB::query("SELECT DISTINCT drg_id, drg.drg_definition, average_covered_charges, average_total_payments, average_medicare_payments, hospital.name, hospital.state, hospital.id FROM financials INNER JOIN hospital ON financials.hospital_id = hospital.id INNER JOIN drg ON financials.drg_id = drg.id WHERE hospitalid=$id", DB::SELECT)->execute()->as_array();
+		return DB::query("SELECT DISTINCT drg_id, drg.drg_definition, average_covered_charges, average_total_payments, average_medicare_payments, hospital.name, hospital.state, hospital.id FROM financials INNER JOIN hospital ON financials.hospital_id = hospital.id INNER JOIN drg ON financials.drg_id = drg.id WHERE hospital_id=$id", DB::SELECT)->execute()->as_array();
 	}
 }
 ?>
